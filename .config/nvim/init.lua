0d54d191f19fd67e750322428d06c4908d1f6e35
Tiemen Waterreus <tiemen.waterreus@grafana.com>
2023-08-07 10:50:40 +0200

    Enable (some) LSPs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -381,12 +381,10 @@ end
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+  clangd = {},
+  gopls = {},
+  tsserver = {},
+  html = { filetypes = { 'html', 'twig', 'hbs' } },
 
   lua_ls = {
     Lua = {
