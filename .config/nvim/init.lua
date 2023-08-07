d22739ff3321a99a610a6618ad957f950133681c
Tiemen Waterreus <tiemen.waterreus@grafana.com>
2023-08-07 17:18:21 +0200

    Add more language servers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -259,7 +259,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'jsonnet' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
@@ -384,6 +384,7 @@ local servers = {
   clangd = {},
   gopls = {},
   tsserver = {},
+  jsonnet_ls = {},
   html = { filetypes = { 'html', 'twig', 'hbs' } },
 
   lua_ls = {
