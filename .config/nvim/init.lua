daf1af01859e4faa47a6ae50f04620eb0670387f
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2023-08-19 10:06:44 +0200

    Enable telescope git_files with untracked files enabled

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -248,9 +248,18 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
-vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+-- show git files fuzzy finder, including untracked files
+local git_files_with_untracked = function()
+  local opts = {
+    show_untracked = true,
+  }
+  require('telescope.builtin').git_files(opts)
+end
+
+vim.keymap.set('n', '<leader>gf', git_files_with_untracked, { desc = 'Search [G]it [F]iles' })
+vim.keymap.set('n', '<C-p>', git_files_with_untracked, { desc = '[S]earch [F]iles' })
+
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<C-p>', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
