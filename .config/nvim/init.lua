efbb0a91dc81070ab50c98a4aa0beaa805bc9ce3
Tiemen Waterreus <tiemen.waterreus@grafana.com>
2025-01-29 14:09:24 +0100

    set scrolloff to 0

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -88,7 +88,7 @@ vim.opt.inccommand = "split"
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 0
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
