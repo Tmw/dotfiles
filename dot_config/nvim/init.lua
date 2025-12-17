cf116aa117799febe1270fc20d52a17cdf10b07f
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2025-12-17 09:21:20 +0100

    Bump deps

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -839,7 +839,6 @@ require("lazy").setup({
   { -- Highlight, edit, and navigate code
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    main = "nvim-treesitter.configs", -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = {
